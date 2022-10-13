aabfa3f326dc31a5c126115c7cc43472444fe3d6
v_liukaijie <v_liukaijie@digitalgd.com.cn>
2022-10-13 18:47:35 +0800

    [+] 完成部分功能

---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,11 @@
+import Theme from 'vitepress/dist/client/theme-default'
+import HelloWord from '../../../src/components/HelloWorld.vue'
+
+export default {
+    ...Theme,
+    // 扩展应用程序实例
+    enhaceAppp({app}) {
+        // 注册组件
+        app.components('HelloWord', HelloWord)
+    }
+}
