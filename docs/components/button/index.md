3a0072640f545c4ddcd1268492f4c0bcb2585741
v_liukaijie <v_liukaijie@digitalgd.com.cn>
2022-10-17 00:34:00 +0800

    [+] 完成部分功能

--- docs/components/button/index.md
+++ docs/components/button/index.md
@@ -1,2 +1,9 @@
 # Button 按钮
-<s-button></s-button>
+<!-- <s-button></s-button> -->
+
+:::demo 使用`size`、`style`属性来定义 Card 的样式。
+```vue
+    <template>
+        <s-button></s-button>
+    </template>
+```
--- src/components/testTsx.tsx
+++ src/components/testTsx.tsx
@@ -1,1 +1,25 @@
-export default () => <div>xxx</div>
+import { defineComponent } from 'vue'
+
+// 创建一个组件有以下几种方式
+
+// 1.函数式组件
+// export () => <div>xxx</div>
+
+// 2.defineComponent
+// export default defineComponent({
+//   render() {
+//     return <div>test</div>
+//   }
+// })
+
+// 3. defineComponent({setup(props, ctx)})
+// 推荐这种写法，对ts支持最好因为它摒弃了this
+// 借助babel-plugin-jsx
+// vue独特的概念： 修饰符，slot，
+export default defineComponent({
+  setup(props, ctx) {
+    return () => {
+      return <div>sss</div>
+    }
+  }
+})
